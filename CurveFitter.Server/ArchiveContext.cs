3a33ad7e9138c3195cef24f9d8a49364313bac11
dariatsvetkova <d.s.tsvetkova@hotmail.com>
2024-01-24 01:05:07 -0500

    start adding sqlite connection

---
+++ CurveFitter.Server/ArchiveContext.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CurveFitter.Server
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public List<Archive> Archives { get; set; }
+    }
+
+    public class Archive
+    {
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public required string FitType { get; set; }
+        public required double[] Equation { get; set; }
+        public required DataPoint[] UserDataPoints { get; set; }
+        public required DataPoint[] FitDataPoints { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+
+        public User User { get; set; }
+    }
+
+    public class ArchiveContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Archive> Archives { get; set; }
+
+        public string DbPath { get; }
+
+        public ArchiveContext()
+        {
+            DbPath = "archive.db";
+        }
+
+        // Configure connection to the database file on disk
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Create tables
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Archive>()
+                .HasKey(a => a.Id);
+
+            modelBuilder.Entity<Archive>()
+                .HasOne(a => a.User)
+                .WithMany(u => u.Archives)
+                .HasForeignKey(a => a.UserId);
+
+            modelBuilder.Entity<Archive>()
+                .Property(a => a.FitType)
+                .IsRequired();
+
+            modelBuilder.Entity<Archive>()
+                .Property(a => a.Equation)
+                .IsRequired();
+
+            modelBuilder.Entity<Archive>()
+                .Property(a => a.UserDataPoints)
+                .IsRequired();
+
+            modelBuilder.Entity<Archive>()
+                .Property(a => a.FitDataPoints)
+                .IsRequired();
+        }
+    }
+}
---
+++ CurveFitter.Server/Controllers/ArchivesController.cs
@@ -0,0 +1,98 @@
+﻿using CurveFitter.Server;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+
+namespace CurveFitter.Server.Controllers
+{
+    public class ArchivesController : Controller
+    {
+        // GET: ArchivesController/Users/5
+        public async ActionResult ActionResult<T>(int id)
+        {
+            await using var db = new ArchiveContext();
+
+            List<Archive> archiveList =
+                await (from archive in db.Archives
+                    join user in db.Users on archive.UserId equals user.Id into tmp
+                    from m in tmp.DefaultIfEmpty()
+
+                    select new Archive
+                    {
+                        Id = sollIst.Id,
+                        CustomerId = sollIst.CustomerId,
+                        User = m,
+                    }
+  ).ToListAsync();
+
+
+            return queryResults.AsAsyncEnumerable();
+        }
+
+        // GET: HomeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HomeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HomeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HomeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
